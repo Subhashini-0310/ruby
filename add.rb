1fd9385aca63e9458b2060bb4c07eee9b81ad60a
Subhashini-0310 <sb031097@gmail.com>
2020-11-20 15:18:47 +0530

    Sb_Upd

--- add.rb
+++ add.rb
@@ -1,19 +1,16 @@
-#Program for addition of numbers.
-
 class Addition
-  def  initialize(arg)    
-     @arr = arg  
-  end 
+  attr_accessor :arr          		#using attr_accessor to access the var
 
   def print
-    @sum = @arr.sum
-    puts 'Result of adding  numbers is : ' + @sum.to_s
+    sum = arr.sum
+    puts 'Result of adding  numbers is : ' + sum.to_s
   end
 end
 
 
-puts 'Enter the numbers for addition : '   
-arg = gets.split(' ').map &:to_i                 #converting array entries separated by space into integers
-obj = Addition.new(arg)                           #initializing class obj
-obj.print                                                     #calling class method
+puts 'Enter the numbers for addition : '
+arg = gets.split(' ').map &:to_i		#converting array entries separated by space into integers	
+obj = Addition.new			#initializing class obj
+obj.arr = arg
+obj.print
 
